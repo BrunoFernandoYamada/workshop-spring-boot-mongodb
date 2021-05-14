19cab12241bd2349f34be6a60b36b30eb532188e
Bruno Yamada <yamada_bruno@hotmail.com>
2021-05-14 19:44:32 -0300

    Obtendo post por Id

---
+++ src/main/java/com/bfyamada/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.bfyamada.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bfyamada.workshopmongo.domain.Post;
+import com.bfyamada.workshopmongo.repository.PostRepository;
+import com.bfyamada.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+
+}
